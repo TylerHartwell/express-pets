3eca1be47235a9269759b9da6d1764f6ad9e9946
TylerHartwell <thartwell37@gmail.com>
2024-04-10 17:17:50 -0700

    open and close overlay

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherPromise = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  )
   const weatherData = await weatherPromise.json()
 
   const ourTemperature = weatherData.properties.periods[0].temperature
   document.querySelector("#temperature-output").textContent = ourTemperature
-
 }
 
 start()
@@ -32,4 +33,22 @@ function handleButtonClick(e) {
       el.style.display = "none"
     }
   })
+}
+
+document.querySelector(".form-overlay").style.display = ""
+
+function openOverlay(el) {
+  document
+    .querySelector(".form-overlay")
+    .classList.add("form-overlay--is-visible")
+}
+
+document
+  .querySelector(".close-form-overlay")
+  .addEventListener("click", closeOverlay)
+
+function closeOverlay() {
+  document
+    .querySelector(".form-overlay")
+    .classList.remove("form-overlay--is-visible")
 }
