e57cc8d7d36c100aa74ae35960d9738ccf465ab9
TylerHartwell <thartwell37@gmail.com>
2024-04-11 14:54:01 -0700

    send relevant pet data to backend

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
 async function start() {
-  const weatherPromise = await fetch(
-    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
-  )
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
 
   const ourTemperature = weatherData.properties.periods[0].temperature
@@ -38,17 +36,34 @@ function handleButtonClick(e) {
 document.querySelector(".form-overlay").style.display = ""
 
 function openOverlay(el) {
-  document
-    .querySelector(".form-overlay")
-    .classList.add("form-overlay--is-visible")
+  document.querySelector(".form-content").dataset.id = el.dataset.id
+  document.querySelector(".form-photo p strong").textContent = el.closest(".pet-card").querySelector(".pet-name").textContent
+  document.querySelector(".form-photo img").src = el.closest(".pet-card").querySelector(".pet-card-photo img").src
+  document.querySelector(".form-overlay").classList.add("form-overlay--is-visible")
 }
 
-document
-  .querySelector(".close-form-overlay")
-  .addEventListener("click", closeOverlay)
+document.querySelector(".close-form-overlay").addEventListener("click", closeOverlay)
 
 function closeOverlay() {
-  document
-    .querySelector(".form-overlay")
-    .classList.remove("form-overlay--is-visible")
+  document.querySelector(".form-overlay").classList.remove("form-overlay--is-visible")
 }
+
+document.querySelector(".form-content").addEventListener("submit", async function (e) {
+  e.preventDefault()
+
+  const userValues = {
+    petId: e.target.dataset.id,
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    secret: document.querySelector("#secret").value,
+    comment: document.querySelector("#comment").value
+  }
+
+  console.log(userValues)
+
+  fetch("/submit-contact", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userValues)
+  })
+})
